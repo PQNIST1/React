a6a5ae9c6e259aba37f8ac2c54dd356569edff83
Quoc Nguyen <nguyen95688@gmail.com>
2023-12-14 19:55:59 +0700

    fix position

--- src/View/Home_Page/Header.js
+++ src/View/Home_Page/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
               <div className="col-3"></div>
               <div className="header-body-left col-7">
                 <button type="button" class="btn text-white">
-                  <i class="fas fa-bed"></i>
+                <svg  xmlns="http://www.w3.org/2000/svg" width="20" height='20' fill="white"  viewBox="0 0 24 24"><path d="M2.75 12h18.5c.69 0 1.25.56 1.25 1.25V18l.75-.75H.75l.75.75v-4.75c0-.69.56-1.25 1.25-1.25zm0-1.5A2.75 2.75 0 0 0 0 13.25V18c0 .414.336.75.75.75h22.5A.75.75 0 0 0 24 18v-4.75a2.75 2.75 0 0 0-2.75-2.75H2.75zM0 18v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 0 18zm22.5 0v3a.75.75 0 0 0 1.5 0v-3a.75.75 0 0 0-1.5 0zm-.75-6.75V4.5a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 2.25 4.5v6.75a.75.75 0 0 0 1.5 0V4.5a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 0 1.5 0zm-13.25-3h7a.25.25 0 0 1 .25.25v2.75l.75-.75h-9l.75.75V8.5a.25.25 0 0 1 .25-.25zm0-1.5A1.75 1.75 0 0 0 6.75 8.5v2.75c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V8.5a1.75 1.75 0 0 0-1.75-1.75h-7z"></path></svg>
                   Lưu trú
                 </button>
                 <span>
@@ -64,17 +64,6 @@ class Header extends Component {
                   Tìm ưu đãi khách sạn, chỗ nghỉ dạng nhà và nhiều hơn nữa...
                 </div>
               </div>
-              <div className="content-between row">
-                <div className="col-3"></div>
-                <div className="content-box col-6">
-                  <div className="box-search form-group search-input">
-                    <i class="fas fa-bed"></i>
-                    <input type="text" placeholder="Bạn muốn đến đâu ?" />
-                  </div>
-                  <div className="box-calendar"></div>
-                  <div className="box-person"></div>
-                </div>
-              </div>
               <div className="content-down row">
                 <div className="col-3"></div>
                 <div className="form-check">
@@ -90,6 +79,17 @@ class Header extends Component {
                 </div>
               </div>
             </div>
+            <div className="content-between row">
+                <div className="col-3"></div>
+                <div className="content-box col-6">
+                  <div className="box-search form-group search-input">
+                  <svg  xmlns="http://www.w3.org/2000/svg" width="20" height='20' fill="black"  viewBox="0 0 24 24"><path d="M2.75 12h18.5c.69 0 1.25.56 1.25 1.25V18l.75-.75H.75l.75.75v-4.75c0-.69.56-1.25 1.25-1.25zm0-1.5A2.75 2.75 0 0 0 0 13.25V18c0 .414.336.75.75.75h22.5A.75.75 0 0 0 24 18v-4.75a2.75 2.75 0 0 0-2.75-2.75H2.75zM0 18v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 0 18zm22.5 0v3a.75.75 0 0 0 1.5 0v-3a.75.75 0 0 0-1.5 0zm-.75-6.75V4.5a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 2.25 4.5v6.75a.75.75 0 0 0 1.5 0V4.5a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 0 1.5 0zm-13.25-3h7a.25.25 0 0 1 .25.25v2.75l.75-.75h-9l.75.75V8.5a.25.25 0 0 1 .25-.25zm0-1.5A1.75 1.75 0 0 0 6.75 8.5v2.75c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V8.5a1.75 1.75 0 0 0-1.75-1.75h-7z"></path></svg>
+                    <input type="text" placeholder="Bạn muốn đến đâu ?" />
+                  </div>
+                  <div className="box-calendar"></div>
+                  <div className="box-person"></div>
+                </div>
+              </div>
           </div>
         </div>
       </>
